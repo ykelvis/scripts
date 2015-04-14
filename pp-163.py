067e91d9ede35965d3dcdc729efdcbd39a3cce0d
ykelvis <yk@archlinuxcn.org>
2015-04-14 22:38:06 +0800

    update

--- pp-163.py
+++ pp-163.py
@@ -21,19 +21,19 @@ def get_entryurl(data):
     a = list(set(a));
     return a;
 
-def download_img(title,link):
-    current_dir = os.getcwd();
+def download_img(title,link,path):
     name = re.findall('(?<=/)\d+.jpg',link);
-    save_path = current_dir + "/" + title + "/" + name[0];
+    save_path = unicode(path + "/" + title.decode('gbk'));
+    file_path = save_path + "/" + name[0];
     try:
-        os.mkdir(current_dir + "/" + title);
+        os.makedirs(path + "/" + title.decode('gbk'));
     except:
         pass;
     try:
         image_data = urllib2.urlopen(link).read();
     except:
         print "Error loading: ", link;
-    image = open(save_path,'wb');
+    image = open(file_path,'wb');
     image.write(image_data);
     image.close();
 
@@ -44,16 +44,16 @@ def get_entry_name(data):
 if __name__ == "__main__":
     _url = sys.argv[1];
     url = "http://" + _url + ".pp.163.com";
-    
+    path = sys.argv[2];
     data = get_page_source(url);
     link = get_entryurl(data);
     for i in link:
         page = get_page_source(i);
         entry_title = get_entry_name(page);
         img_link = get_imgurl(page);
-        print "Downloading blog: ", link.index(i) + 1, " of ", len(link);
+        print "Downloading ", entry_title," ", link.index(i) + 1, " of ", len(link);
         for j in img_link:
             print "Downloading img: ", img_link.index(j) + 1, " of ", len(img_link);
-            download_img(entry_title[0].decode('GBK'),j)
+            download_img(entry_title[0],j,path)
     #print a[0].decode('utf-8')
 

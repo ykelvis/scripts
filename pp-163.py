21d3b011a5abf57867f7764eaee969c2ce42b8d0
ykelvis <yk@archlinuxcn.org>
2015-04-14 22:01:47 +0800

    update

--- pp-163.py
+++ pp-163.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 import urllib2
 import re
@@ -5,7 +6,10 @@ import os
 import sys
 
 def get_page_source(url):
-    data = urllib2.urlopen(url).read();
+    try:
+        data = urllib2.urlopen(url).read();
+    except:
+        print "Error loading: ",url
     return data;
 
 def get_imgurl(data):
@@ -20,29 +24,36 @@ def get_entryurl(data):
 def download_img(title,link):
     current_dir = os.getcwd();
     name = re.findall('(?<=/)\d+.jpg',link);
-    print name;
     save_path = current_dir + "/" + title + "/" + name[0];
     try:
         os.mkdir(current_dir + "/" + title);
     except:
-        print "folder exists"
         pass;
-    image_data = urllib2.urlopen(link).read();
+    try:
+        image_data = urllib2.urlopen(link).read();
+    except:
+        print "Error loading: ", link;
     image = open(save_path,'wb');
     image.write(image_data);
     image.close();
 
+def get_entry_name(data):
+    a = re.findall("(?<=title>).*(?=</title)",data)
+    return a;
+
 if __name__ == "__main__":
     _url = sys.argv[1];
-    save_dir = sys.argv[2];
     url = "http://" + _url + ".pp.163.com";
-    print url;
-
+    
     data = get_page_source(url);
     link = get_entryurl(data);
     for i in link:
         page = get_page_source(i);
+        entry_title = get_entry_name(page);
         img_link = get_imgurl(page);
+        print "Downloading blog: ", link.index(i) + 1, " of ", len(link);
         for j in img_link:
-            print j;
-            download_img(save_dir,j)
+            print "Downloading img: ", img_link.index(j) + 1, " of ", len(img_link);
+            download_img(entry_title[0].decode('GBK'),j)
+    #print a[0].decode('utf-8')
+

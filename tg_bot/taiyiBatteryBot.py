7ebc49ca23d3a1fda0e572cc3322222dc4426947
yk <yk@archlinuxcn.org>
2016-07-18 15:18:18 +0800

    up taiyi

--- tg_bot/taiyiBatteryBot.py
+++ tg_bot/taiyiBatteryBot.py
@@ -1,16 +1,31 @@
 #!/usr/local/bin/python3
 # coding=utf-8
-import telebot, logging, datetime, random, sys, time
-from telebot import types
+import logging, sqlite3, json, sys, random, time
+from uuid import uuid4
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+from telegram import ParseMode, InlineQueryResultArticle, InputTextMessageContent
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
-token = sys.argv[1]
-bot = telebot.AsyncTeleBot(token)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 strip = lambda a:a.lstrip(a.split()[0]).lstrip().rstrip()
+
+def wrapper(*args,**kwargs):
+    def w(fn):
+        def ww(bot,update):
+            logging.info('user: {} command: {}'.format(update.message.from_user.username,update.message.text))
+            text = fn(bot,update)
+            logging.info('response: {}'.format(text))
+            if kwargs.get('reply_to',None):
+                kwargs['reply_to'] = update.message.message_id
+            bot.sendMessage(update.message.chat_id,reply_to_message_id=kwargs['reply_to'],text=text,parse_mode=kwargs.get('parse_mode',None), disable_web_page_preview=kwargs.get('disable_preview',None))
+            return 0
+        return ww
+    return w
 
 timeend = 78600
 timestart = 32400
+emoji = ['( ´_ゝ`)','（〃｀д´ )( ´ｪ`)','（〃｀д´ ) 🍼','（〃｀д´ ) ☔️','( ･ั﹏･ั )','( ´∀｀ )','( ﾟдﾟ )','(・Д・)ノ','(^o^)丿','(๑´•.̫ • `๑)','(゜-゜)','(｡ŏ﹏ŏ)','（ ・∀・）','✌(´◓ｑ◔｀)✌﻿','_:(´ཀ`」 ∠):_','（￣へ￣）','(｡•ˇ‸ˇ•｡)﻿','(　д ) ﾟ ﾟ','((((；ﾟДﾟ)))))))','(ﾟДﾟ≡ﾟДﾟ)','Σ(っ°Д°;)っ',' ༼ つ ◕_◕ ༽つ','(;´༎ຶД༎ຶ`)','｡･ﾟ･(ﾉД`)･ﾟ･｡','ヽ(；▽；)ノ','_(:3」 ∠)_','( ´◔ ‸◔`)﻿','╮( ๑╹,◡╹ ๑ ) ╭','(つд⊂)','( ´∀`)σ)Д` )','(╯°Д°)╯','( ͡° ͜ʖ ͡° )','¯\_(ツ)_/¯']
 
 def calBattery():
     global timeend,timesatart
@@ -23,7 +38,6 @@ def calBattery():
         bat = (d - diff * diff / d) / d
         return "太医电量剩余: {:.2%}".format(bat)
 
-@bot.inline_handler(lambda query: query.query == '+1s')
 def query_charge(inline_query):
     global timeend
     timeend +=1
@@ -34,37 +48,36 @@ def query_charge(inline_query):
     r1 = types.InlineQueryResultArticle('taiyi', text, text)
     bot.answer_inline_query(inline_query.id, [r1],cache_time=1)
 
-@bot.inline_handler(lambda query: query.query == '')
-def query_battery(inline_query):
+def inline_battery(bot,update):
+    global emoji
+    query = update.inline_query.query
+    results = list()
+    logging.info('inline {} from {}'.format('no content' if update.inline_query.query == '' else update.inline_query.query,update.inline_query.from_user.username))
     try:
-        emoji = ['( ´_ゝ`)','（〃｀д´ )( ´ｪ`)','（〃｀д´ ) 🍼','（〃｀д´ ) ☔️','( ･ั﹏･ั )','( ´∀｀ )','( ﾟдﾟ )','(・Д・)ノ','(^o^)丿','(๑´•.̫ • `๑)','(゜-゜)','(｡ŏ﹏ŏ)','（ ・∀・）','✌(´◓ｑ◔｀)✌﻿','_:(´ཀ`」 ∠):_','（￣へ￣）','(｡•ˇ‸ˇ•｡)﻿','(　д ) ﾟ ﾟ','((((；ﾟДﾟ)))))))','(ﾟДﾟ≡ﾟДﾟ)','Σ(っ°Д°;)っ',' ༼ つ ◕_◕ ༽つ','(;´༎ຶД༎ຶ`)','｡･ﾟ･(ﾉД`)･ﾟ･｡','ヽ(；▽；)ノ','_(:3」 ∠)_','( ´◔ ‸◔`)﻿','╮( ๑╹,◡╹ ๑ ) ╭','(つд⊂)','( ´∀`)σ)Д` )','(╯°Д°)╯','( ͡° ͜ʖ ͡° )','¯\_(ツ)_/¯']
         c = calBattery()
         a = random.choice(emoji)
         c = c + ' ' + str(a)
-        r1 = types.InlineQueryResultArticle('taiyi', c, c)
-        r2 = types.InlineQueryResultArticle('agent','特工电量剩余：+∞', '特工电量剩余：+∞')
-        r = [r1,r2]
-        j = 0
-        for i in emoji:
-            r.append(types.InlineQueryResultArticle(str(j),i,i))
-            j+=1
-        bot.answer_inline_query(inline_query.id, r,cache_time=5)
+        results.append(InlineQueryResultArticle(id=uuid4(),title=c,input_message_content=InputTextMessageContent(c)))
+        results.append(InlineQueryResultArticle(id=uuid4(),title='特工电量剩余：+∞',input_message_content=InputTextMessageContent('特工电量剩余：+∞')))
+        [results.append(InlineQueryResultArticle(id=uuid4(),title=x,input_message_content=InputTextMessageContent(x))) for x in emoji]
+        bot.answerInlineQuery(update.inline_query.id, results=results,cache_time=5)
+        logging.info('return succ, {}'.format(c))
     except Exception as e:
         print(e)
 
-@bot.message_handler(commands=['taiyi'])
-def taiyi(message):
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def taiyi(bot,update):
     m = calBattery()
-    bot.reply_to(message, m)
+    return m
 
-@bot.message_handler(commands=['roll'])
-def roll(message):
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def roll(bot,update):
     a = random.randrange(1,101)
-    bot.reply_to(message,a)
+    return a
 
-@bot.message_handler(commands=['dice'])
-def dice(message):
-    text = strip(message.text)
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def dice(bot,update):
+    text = strip(update.message.text)
     try:
         text = text.replace(" ","")
         x,y = text.split(",")
@@ -72,33 +85,51 @@ def dice(message):
         if a < b:
             b = b + 1
             r = random.randrange(a,b)
-            bot.reply_to(message,r)
+            return r
         else:
-            bot.reply_to(message,"invalid")
+           return "usage: /dice 1,6"
     except:
-        bot.reply_to(message,"usage: /dice 1,6")
+       return "usage: /dice 1,6"
 
-@bot.message_handler(commands=['choice'])
-def choice(message):
-    text = strip(message.text)
-    try:
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def choice(bot,update):
+    text = strip(update.message.text)
+    if text == '':
+        return 'usage: /choice a,b,c,d'
+    else:
         a = text.split(",")
         r = random.choice(a)
-        bot.reply_to(message,r)
-    except:
-        bot.reply_to(message,"usage: /choice c1,c2,c3")
+        return r
 
-@bot.message_handler(commands=['say'])
-def say(message):
-    try:
-        x,y = message.text.split(" ")
-        bot.reply_to(message,y)
-    except:
-        bot.reply_to(message,"usage: /say blabla")
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def say(bot,update):
+    text = strip(update.message.text)
+    if text == '':
+        return 'usage: /say blabla'
+    else:
+        return text
 
-@bot.message_handler(commands=['test'])
-def test(message):
-    text = strip(message.text)
-    bot.reply_to(message,text)
+@wrapper(disable_preview=True,parse_mode=None,reply_to=True)
+def test(bot,update):
+    text = update.message.text
+    return text
 
-bot.polling()
+def main(token):
+    TOKEN = token
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('roll',roll))
+    dp.add_handler(CommandHandler('taiyi',taiyi))
+    dp.add_handler(CommandHandler('choice',choice))
+    dp.add_handler(CommandHandler('say',say))
+    dp.add_handler(CommandHandler('dice',dice))
+    dp.add_handler(CommandHandler('test',test))
+
+    dp.add_handler(InlineQueryHandler(inline_battery))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    t = sys.argv[1]
+    main(t)

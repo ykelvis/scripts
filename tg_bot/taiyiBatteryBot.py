1d6833f8aebb13129293e4b9556de6423adab505
yk <yk@archlinuxcn.org>
2016-07-19 16:01:48 +0800

    add space

--- tg_bot/taiyiBatteryBot.py
+++ tg_bot/taiyiBatteryBot.py
@@ -56,6 +56,14 @@ def inline_battery(bot,update):
         ret = inline_charge()
         results = [InlineQueryResultArticle(id=1,title=ret,input_message_content=InputTextMessageContent(ret))]
         bot.answerInlineQuery(update.inline_query.id, results=results, cache_time=1)
+    elif update.inlin_query.query != '':
+        q = update.inline_query.query
+        ret = ''
+        c = [' ','']
+        for i in q:
+            ret = ret + i + random.choice(c)
+        results.append(InlineQueryResultArticle(id=1,title=ret,input_message_content=InputTextMessageContent(ret)))
+        bot.answerInlineQuery(update.inline_query.id, results=results,cache_time=1)
     else:
         try:
             c = calBattery()
